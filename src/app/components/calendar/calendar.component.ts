07f66e1a4b98678e27c1167a6d7066549c185d2f
Hesam Kashefi <hkashefi7@outlook.com>
2024-12-05 20:37:05 +0330

    added generate month

---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { DisplayDatePipe } from '../../pipes/DisplayDate.pipe';
+
+@Component({
+  selector: 'app-calendar',
+  imports: [
+    DisplayDatePipe
+  ],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.scss'
+})
+export class CalendarComponent implements OnInit {
+
+
+  currentMonth?: Date[][];
+  currentMonthIndex?: number;
+
+  ngOnInit(): void {
+    const now = new Date();
+    const month = new Date(now.getFullYear(), now.getMonth() - 2, 1);
+    this.currentMonthIndex = month.getMonth();
+    this.currentMonth = this.generateMonth(month);
+  }
+
+
+  generateMonth(date: Date): Date[][] {
+    const month: { row: number, date: Date }[] = [];
+
+    const oneDayInMiliseconds = 24 * 60 * 60 * 1000;
+    const weekDayForTheFirstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    const lastDayOfPreviousMonth = new Date(date.getFullYear(), date.getMonth(), 0);
+
+    for (let dayInRow = weekDayForTheFirstDayOfMonth - 1; dayInRow >= 0; dayInRow--) {
+      month.push({
+        row: 0,
+        date: new Date(lastDayOfPreviousMonth.getTime() - oneDayInMiliseconds * dayInRow)
+      })
+    }
+
+    const tempDate = new Date(date.getFullYear(), date.getMonth(), 1);
+    while (tempDate.getMonth() === date.getMonth()) {
+      month.push({
+        row: Math.floor(month.length / 7),
+        date: new Date(tempDate.getTime())
+      });
+      tempDate.setDate(tempDate.getDate() + 1);
+    }
+
+    while (tempDate.getDay() !== 0) {
+      month.push({
+        row: Math.floor(month.length / 7),
+        date: new Date(tempDate.getTime())
+      })
+      tempDate.setDate(tempDate.getDate() + 1);
+    }
+
+    const dic: { [row: number]: Date[] } = {};
+    month.reduce((rv, x) => {
+      (rv[x.row] = rv[x.row] || []).push(x.date);
+      return rv;
+    }, dic);
+
+    return Object.keys(dic).map(x => dic[+x]);
+  }
+
+  isInTheCurrentMonth(date: Date) {
+    return date.getMonth() === this.currentMonthIndex;
+  }
+
+  /**
+   * calculates the number of days in the specified month of year
+   * @param year year
+   * @param month month (0 for janurary to 11 for december)
+   * @returns number of days in the month
+   */
+  private getNumberOfDaysInMonth(year: number, month: number) {
+    // using 0 as the day, will cause to create a date that points to the last
+    // day of the previous month
+    // so we also need to increment the month by one
+    // so the result is last day of the current month
+    return new Date(year, month + 1, 0).getDate();
+  }
+
+}
---
+++ src/app/pipes/DisplayDate.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, type PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'appDisplayDate',
+})
+export class DisplayDatePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+
+    if (value instanceof Date) {
+      return new Date(value).getDate();
+    }
+
+    return value;
+  }
+
+}
